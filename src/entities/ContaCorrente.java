4395150848b0b9c4040d668d8616cb20e234a7d0
dantefleschpereira <48799984+dantefleschpereira@users.noreply.github.com>
2021-12-30 18:09:46 -0300

    update

--- src/entities/ContaCorrente.java
+++ src/entities/ContaCorrente.java
@@ -1,5 +1,20 @@
 package entities;
 
 public class ContaCorrente {
-
+	
+	public int agencia;
+	public int numero;
+	public double saldo;
+	
+	public void sacar() {
+		
+	}
+	
+	public void depositar() {
+		
+	}
+	
+	public void transferir() {
+		
+	}
 }
--- src/entities/ContaPoupanca.java
+++ src/entities/ContaPoupanca.java
@@ -2,4 +2,19 @@ package entities;
 
 public class ContaPoupanca {
 
+	public int agencia;
+	public int numero;
+	public double saldo;
+
+	public void sacar() {
+
+	}
+
+	public void depositar() {
+
+	}
+
+	public void transferir() {
+
+	}
 }
